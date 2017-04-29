44435f7e1f81e7970234dd69cd14df274f30179e
LingnanLemon <yuanln9281@gmail.com>
2017-04-29 16:55:25 -0400

    add naive_bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,20 +14,16 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+gnb_ins = GaussianNB()
+gnb_ins.fit(features_train, labels_train)
 
-
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
+res = gnb_ins.predict(features_test)
+print gnb_ins.score(features_test, labels_test)
 
